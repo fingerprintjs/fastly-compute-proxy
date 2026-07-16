13f246e51d3c7dc6be3f4d18b2da56cec62453ab
Eray Aydın <eray.aydin@fingerprint.com>
2026-07-16 16:12:10 +0300

    fix: replace __dirname with ESM-compatible

--- scripts/api/deployPackage.ts
+++ scripts/api/deployPackage.ts
@@ -1,10 +1,14 @@
 import { createClient } from '../utils/createClient.ts'
 import * as fs from 'fs'
+import * as path from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export async function deployPackage(service_id: string, versionId: number) {
   return createClient('package').putPackage({
     version_id: versionId,
     service_id,
-    _package: fs.createReadStream(__dirname + '/../../pkg/fingerprint-fastly-compute-proxy-integration.tar.gz'),
+    _package: fs.createReadStream(path.join(__dirname, '../../pkg/fingerprint-fastly-compute-proxy-integration.tar.gz')),
   })
 }
